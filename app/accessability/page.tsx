c18b943bf6a39279a617ae6bc60f3db45b37de64
jevitale14-droid <jevitale14@gmail.com>
2025-11-04 14:26:20 -0600

    Create page.tsx

---
+++ app/accessability/page.tsx
@@ -0,0 +1,9 @@
+import { AccessibilityPanel } from "@/components/accessibility-panel"
+
+export default function AccessibilityPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <AccessibilityPanel />
+    </div>
+  )
+}
