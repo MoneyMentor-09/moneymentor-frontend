2cb989159e7756d86e0c1a893827b1deb49cf217
Chijioke Okogwu <cwokogwu@gmail.com>
2025-11-17 12:31:30 -0600

    Removed the settings button in the dashboard

--- components/dashboard-layout.tsx
+++ components/dashboard-layout.tsx
@@ -570,10 +570,10 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
                     </Link>
                   </DropdownMenuItem>
 
-                  {/* Settings Link */}
+                  {/* Settings Link
                   <DropdownMenuItem>
                     <Settings className="mr-2 h-4 w-4" /> Settings
-                  </DropdownMenuItem>
+                  </DropdownMenuItem> */}
 
                   <DropdownMenuSeparator />
 
